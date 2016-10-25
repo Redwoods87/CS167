476a910ec5dad7476a498326caf7b6a035d626da
Redwoods87 <johnsoam1987@gmail.com>
2016-10-24 21:15:43 -0700

    final modifications to mysery and grep

--- grep.py
+++ grep.py
@@ -2,26 +2,36 @@
 Author username(s): johnsoam
 Date: 10/24/16
 Assignment/problem number: Exam 2
-Assignment/problem title: Prgoramming
+Assignment/problem title: Programming
 """
 
 def grep (searchstring, filename):
+    """
+    Prints lines of a text file containing a specified substring.
+    
+    Parameters:
+        searchstring: a string to be found in the given text file
+        filename: the name of a file to search in
+    
+    Returns: None
+    
+    Preconditions: searchstring must be a string of characters; filename must be a string of the name of a text file in the same directory, including the file extension.
+    
+    Postconditions: will print each line of the file containing the searchstring as a string; each line will be printed only once, even if it contains the searchstring multiple times.
+    """
     
     textFile = open(filename, "r", encoding = "utf-8")
    
     for line in textFile:
-        linePrinted = False 
+        lineAlreadyPrinted = False 
         for i in range(len(line) - len(searchstring) + 1):
-            if line[i:i+len(searchstring)] == searchstring and linePrinted == False:
+            if line[i:i+len(searchstring)] == searchstring and lineAlreadyPrinted == False:
                 print(line)
-                linePrinted = True     
+                lineAlreadyPrinted = True     
                 
     textFile.close()        
                 
 if __name__ == "__main__":
-    grep ("prefer not to", "bartleby.txt")
+    grep ("prefer not to", "bartleby.txt")  
     
-    
-    
-    #text = textFile.read()
-    #textFile.close()
+    # Current test uses Herman Melville's "Bartleby, The Scrivener" at "http://www.gutenberg.org/cache/epub/11231/pg11231.txt".
--- mystery.py
+++ mystery.py
@@ -7,19 +7,20 @@ Assignment/problem title: Decoding code
 
 def commonChars (stringOne, stringTwo):
     """
-    Finds the common characters at the beginning of two strings.
+    Finds the common characters at the beginnings of two strings.
     
     Parameters:
         stringOne: a string 
         stringTwo: a second string
     
-    Returns: a string of characters identical to characters at the beginnings of stringOne and stringTwo
+    Returns: a string of characters identical to the matching characters at the beginnings of stringOne and stringTwo
     
     Preconditions: both parameters must be strings
-    Postconditions: a single string, shorter than or equal to the length of the shorter input string, consisting of characters that appear at the beginning of both strings    
+    
+    Postconditions: will return a single string, shorter than or equal to the length of the shorter input string, consisting of characters that appear at the beginnings of both strings    
     """
     
-    endIndex = 0
+    endIndex = 0        # the index following the final matching character
     shortLen = min(len(stringOne), len(stringTwo))
     while endIndex < shortLen and stringOne[endIndex] == stringTwo[endIndex]:
         endIndex = endIndex + 1
